d6e50fef8622c91b65d40a084edf8e431121b26f
Antonio Casero Palmero <antonio.palmero@here.com>
2018-06-28 11:15:13 +0200

    Starting with Reactive

---
+++ README.md
@@ -0,0 +1,5 @@
+1- node
+2- yarn : npm install -g yarn
+3- yarn global add live-server
+    - go to terminal: $live-server public 
+4-.babeljs.io --> Compiler
---
+++ public/scripts/app.js
@@ -0,0 +1,14 @@
+console.log("is running")
+
+//JSX - JavaScripy XML
+
+//var template = <p>This is JSX from app.js!</p>;
+var template = React.createElement(
+    "h1",
+    null,
+    "This is JSX from app.js!"
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
